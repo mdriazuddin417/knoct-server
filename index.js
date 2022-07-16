79aa6526203c776088ef28cb80ebc9494e1c897e
mdriazuddin417 <mdriazuddin417@gmail.com>
2022-07-16 07:33:08 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+app.use(cors());
+app.use(express.json());
+
+const uri =
+  "mongodb+srv://knoctTask:M5mjp83hdMi4WHBW@cluster0.icbjq.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    await client.connect();
+    const reviewsCollection = client.db("knoctTask").collection("reviews");
+
+    app.get("/reviews", async (req, res) => {
+      const result = await reviewsCollection.find({}).toArray();
+      res.send(result);
+    });
+
+    app.get("/review", async (req, res) => {
+      const page = parseInt(req.query.page);
+      const size = parseInt(req.query.size);
+      const query = {};
+      const cursor = reviewsCollection.find(query);
+      let products;
+      if (page || size) {
+        products = await cursor
+          .skip(page * size)
+          .limit(size)
+          .toArray();
+      } else {
+        products = await cursor.toArray();
+      }
+
+      res.send(products);
+    });
+    app.get("/productCount", async (req, res) => {
+      const count = await reviewsCollection.estimatedDocumentCount();
+      res.send({ count });
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+app.get("/", (req, res) => {
+  res.send("My first data check it");
+});
+
+app.listen(port, () => {
+  console.log("Example port check", port);
+});
+module.exports = app;
